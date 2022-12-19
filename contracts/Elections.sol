630427f93dbfe170ed1199617c4110736632b293
YashTelkhade <yptelkhade@gmail.com>
2022-12-19 11:21:28 -0600

    first commit with Smoke Test

---
+++ contracts/Elections.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.16;
+
+contract Elections{
+    // Store Candidate
+    // Read Candidate
+    string public candidate;
+    // Constructor
+
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+
+}
